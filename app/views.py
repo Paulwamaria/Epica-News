68817749e68b329b2777c213ec60bde932f463fc
Paulwamaria <paulwamaria@gmail.com>
2019-09-07 14:24:04 +0300

    Add a for loop to display top entertainment news.

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .requests import get_news
 
 
 @app.route('/')
@@ -11,4 +12,10 @@ def index():
     title = "Welcome to Epica News Highlights"
     message = 'Welcome to Epica News'
 
-    return render_template('index.html', text = message, tittle = title )
+    # Getting popular movie
+    news_highlights = get_news('entertainment')
+    print(news_highlights)
+   
+
+
+    return render_template('index.html',text=message, title = title, entertainment = news_highlights)
