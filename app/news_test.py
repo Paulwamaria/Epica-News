061a41db8510f0cd49f1d9a18572be0c74510ddd
Paulwamaria <paulwamaria@gmail.com>
2019-09-09 14:49:55 +0300

    update all

--- app/news_test.py
+++ app/news_test.py
@@ -14,7 +14,7 @@ class NewsTest(unittest.TestCase):
         '''
 
 
-        self.new_news = News('Fox','crazy','The Amazing Lion','A lion that saves a gazelle from an alligator','"https://i.kinja-img.com/gawker-media/image/upload/s--H8pqYMUW--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/ug34lxszlekl8efydtj3.png','image','30/06/2019',)
+        self.new_news = News('Fox','crazy','The Amazing Lion','A lion that saves a gazelle from an alligator','https://i.kinja-img.com/gawker-media/image/upload/s--H8pqYMUW--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/ug34lxszlekl8efydtj3.png','image','30/06/2019')
         
     def test_instance(self):
         self.assertTrue(isinstance(self.new_news,News))
@@ -29,7 +29,7 @@ class SourceTest(unittest.TestCase):
         '''
 
 
-        self.new_source = Source('Fox','crazy','A lion that saves a gazelle from an alligator')
+        self.new_source = Source('Fox','crazy','A lion that saves a gazelle from an alligator','https://i.kinja-img.com/gawker-media/image/upload/s--H8pqYMUW--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/ug34lxszlekl8efydtj3.png','image','30/06/2019')
         
     def test_instance(self):
         self.assertTrue(isinstance(self.new_source,Source))
